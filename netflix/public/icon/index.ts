f0df96488de9ca89a52fc16da13c604d9dac4d7a
Rose <120544840+Rose-my@users.noreply.github.com>
2024-05-06 22:44:48 +0900

    etc: 필요한 아이콘 추가

---
+++ netflix/public/icon/index.ts
@@ -0,0 +1,8 @@
+export { default as ComingsoonIcon } from './comingsoon.svg';
+export { default as DownloadIcon } from './download.svg';
+export { default as FindIcon } from './find.svg';
+export { default as HomeIcon } from './home.svg';
+export { default as MoreIcon } from './more.svg';
+export { default as NetflixIcon } from './netflix.svg';
+export { default as PlayIcon } from './play.svg';
+export { default as PlusIcon } from './plus.svg';
