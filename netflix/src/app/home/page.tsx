9258c05d0627ac64a9037ae94a6db3eddd6d57da
Rose <120544840+Rose-my@users.noreply.github.com>
2024-05-08 00:35:06 +0900

    design: PlayBtn UI

--- netflix/src/app/home/page.tsx
+++ netflix/src/app/home/page.tsx
@@ -1,11 +1,11 @@
-import FooterNav from "@/components/common/FooterNav";
-
+import FooterNav from '@/components/common/FooterNav';
+import PlayBtn from '@/components/common/PlayBtn';
 
 export default function page() {
   return (
     <section>
       <p className="fonts-headline">home페이지의 컴포넌트들이 올 자리, 리액트의 pages폴더의 역할</p>
-      <div className="bg-white rounded-md fonts-btn h-[10px]">Play</div>
+      <PlayBtn width={303}></PlayBtn>
       <FooterNav />
     </section>
   );
