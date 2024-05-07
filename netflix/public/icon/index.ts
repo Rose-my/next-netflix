40274c59e555a1ff61b4238205d5226973af2ea6
Rose <120544840+Rose-my@users.noreply.github.com>
2024-05-07 18:08:59 +0900

    etc: 아이콘 네이밍변경 & import 환경

--- netflix/public/icon/index.ts
+++ netflix/public/icon/index.ts
@@ -1,8 +1,11 @@
 export { default as ComingsoonIcon } from './comingsoon.svg';
 export { default as DownloadIcon } from './download.svg';
-export { default as FindIcon } from './find.svg';
-export { default as HomeIcon } from './home.svg';
-export { default as MoreIcon } from './more.svg';
+export { default as HomeActiveIcon } from './homeActive.svg';
+export { default as HomeDeactiveIcon } from './homeDeactive.svg';
+export { default as SearchActiveIcon } from './searchActive.svg';
+export { default as SearchDeactiveIcon } from './searchDeactive.svg';
+export { default as InfoIcon } from './info.svg';
 export { default as NetflixIcon } from './netflix.svg';
 export { default as PlayIcon } from './play.svg';
 export { default as PlusIcon } from './plus.svg';
+export { default as MoreIcon } from './more.svg';
