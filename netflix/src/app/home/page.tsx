391857a4caeb8acb9734916df2be44234998cfc5
Rose <120544840+Rose-my@users.noreply.github.com>
2024-05-07 23:32:53 +0900

    design: FooterNav 컴포넌트 하단에 고정

--- netflix/src/app/home/page.tsx
+++ netflix/src/app/home/page.tsx
@@ -1,11 +1,12 @@
-import FooterIcon from '@/components/common/FooterIcon';
+import FooterNav from "@/components/common/FooterNav";
+
 
 export default function page() {
   return (
     <section>
       <p className="fonts-headline">home페이지의 컴포넌트들이 올 자리, 리액트의 pages폴더의 역할</p>
-      <div className="bg-white rounded-md fonts-btn h-[700px]">Play</div>
-      <FooterIcon />
+      <div className="bg-white rounded-md fonts-btn h-[10px]">Play</div>
+      <FooterNav />
     </section>
   );
 }
