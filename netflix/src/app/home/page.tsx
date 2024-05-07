5392493e2b2cc0a4d7e3b253375b9cc2b6a07a7b
Rose <120544840+Rose-my@users.noreply.github.com>
2024-05-07 12:14:08 +0900

    feat: 폴더추가

--- netflix/src/app/home/page.tsx
+++ netflix/src/app/home/page.tsx
@@ -3,7 +3,7 @@ import FooterIcon from '@/components/common/FooterIcon';
 export default function page() {
   return (
     <section>
-      <p className="fonts-headline">Test</p>
+      <p className="fonts-headline">home페이지의 컴포넌트들이 올 자리, 리액트의 pages폴더의 역할</p>
       <div className="bg-white rounded-md fonts-btn h-[700px]">Play</div>
       <FooterIcon />
     </section>
