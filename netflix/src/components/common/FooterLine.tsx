e6fef1968097c5f4a0fc452bff6981b9c4ae6d27
Rose <120544840+Rose-my@users.noreply.github.com>
2024-05-07 23:34:25 +0900

    design: FooterNav에 쓰이는 FooterBtn UI

--- netflix/src/components/common/FooterLine.tsx
+++ netflix/src/components/common/FooterLine.tsx
@@ -1,6 +1,6 @@
 export default function FooterLine() {
   return (
-    <div className="relative">
+    <div className="absolute h-[31.7px] bottom-0">
       <div className="left-0 top-[-0.91px] absolute" />
       <div className="w-[121.38px] h-[4.53px] left-[126.81px] top-[18.12px] absolute bg-white rounded-[90.58px]" />
     </div>
