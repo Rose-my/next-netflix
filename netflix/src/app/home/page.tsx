f42a4c4ecc009a946af1693872d3756f8e2e3d56
Rose <120544840+Rose-my@users.noreply.github.com>
2024-05-08 11:41:38 +0900

    design: movies-wrapper 공통 컴포넌트UI

--- netflix/src/app/home/page.tsx
+++ netflix/src/app/home/page.tsx
@@ -1,12 +1,20 @@
 import FooterNav from '@/components/common/FooterNav';
 import PlayBtn from '@/components/common/PlayBtn';
+import Wrapper from '@/components/home/Wrapper';
 
 export default function page() {
   return (
-    <section>
-      <p className="fonts-headline">home페이지의 컴포넌트들이 올 자리, 리액트의 pages폴더의 역할</p>
+    <div className="pb-8">
+      <header className="fonts-headline">home페이지의 헤드라인</header>
       <PlayBtn width={303}></PlayBtn>
+      <PlayBtn width={110.62}></PlayBtn>
+      <Wrapper title="Continue Watching for Emenalo" />
+      <Wrapper title="Popular on Netflix" />
+      <Wrapper title="Trending Now" />
+      <Wrapper title="Top 10 in Nigeria Today" />
+      <Wrapper title="My List" />
+      <Wrapper title="African Movies" />
       <FooterNav />
-    </section>
+    </div>
   );
 }
