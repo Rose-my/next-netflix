866306f730a0813027148e5d68531ea7e746b42e
Rose <120544840+Rose-my@users.noreply.github.com>
2024-05-15 14:17:11 +0900

    feat: 검색 페이지 기능구현

--- netflix/src/app/search/page.tsx
+++ netflix/src/app/search/page.tsx
@@ -1,26 +1,29 @@
-import FooterNav from '@/components/common/FooterNav';
-import Search from '@/components/search/SearchBar';
-import Wrapper from '@/components/search/Wrapper';
+'use client';
 
-export default function page() {
-  // const wrapperItems = [
-  //   { title: 'Continue Watching for Emenalo', fetchType: 'topRated' },
-  //   { title: 'Popular on Netflix', fetchType: 'popular' },
-  //   { title: 'Trending Now', fetchType: 'trending' },
-  //   { title: 'Top Rated', fetchType: 'topRated' },
-  //   { title: 'Horror Movies', fetchType: 'horrorMovies' },
-  // ];
+import { useState } from 'react';
+import FooterNav from '@/components/common/FooterNav';
+import SearchBar from '@/components/search/SearchBar';
+import Movies from '@/components/search/Movies';
+import { getSearchedMovies } from '@/api/getSearchedMovies';
+
+export default function Page() {
+  const [searchedMovies, setSearchedMovies] = useState([]);
+
+  const handleSearch = async (query: string) => {
+    try {
+      const data = await getSearchedMovies(query);
+      setSearchedMovies(data.results);
+    } catch (error) {
+      console.error('에러 발생:', error);
+    }
+  };
 
   return (
     <section className="flex flex-col pt-11">
-      <Search />
-      <p className=" py-5 pl-2.5 fonts-bigtitle">Top Searches</p>
-      <section className="flex flex-col overflow-auto">
-        <Wrapper />
-        {/* {wrapperItems.map((item, index) => (
-        <Wrapper key={index} title={item.title} fetchType={item.fetchType} />
-      ))} */}
-      </section>
+      <SearchBar onSearch={handleSearch} />
+      <p className="py-5 pl-2.5 fonts-bigtitle">Top Searches</p>
+      <div className="movies-container"></div>
+      <Movies searchedMovies={searchedMovies} />
       <FooterNav />
     </section>
   );
--- netflix/src/components/search/SearchBar.tsx
+++ netflix/src/components/search/SearchBar.tsx
@@ -1,22 +1,34 @@
 import { SmallSearchIcon, SmallXIcon } from '@/public/icon';
+import { useState } from 'react';
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
 
-export default function SearchBar() {
+export default function SearchBar({ onSearch }: SearchBarProps) {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+  }
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setSearchQuery(e.target.value);
+    onSearch(e.target.value);
+  }
+
   return (
-    <form className="flex justify-between w-full h-[52px] py-4 px-5 bg-search-gray">
-      <div className="flex items-center  ">
-        <SmallSearchIcon className="pr-1" />
-        <input
-          type="text"
-          className="block fonts-input bg-transparent w-[270px] px-2 focus:outline-none"
-          placeholder=" Search for a show, movie, genre, e.t.c. "
-        />
-      </div>
+    <form
+      className="flex justify-between items-center w-full h-[52px] py-4 px-5 bg-search-gray"
+      onSubmit={handleSubmit}>
+      <SmallSearchIcon className="pr-1" />
+      <input
+        type="text"
+        className="block fonts-input bg-transparent px-2 focus:outline-none w-screen"
+        placeholder="Search for a show, movie, genre, etc."
+        value={searchQuery}
+        onChange={handleInputChange}
+      />
       <SmallXIcon className="items-center" />
     </form>
   );
 }
-// className =
-//   'w-[27rem] h-[3.1rem] bg-searchBar-main text-button-main placeholder-current text-[1.5rem] searchBar-text placeholder-text-center';
-// placeholder = 'Search for a show, movie, e.t.c.';
-// value = { searchText };
-// onChange = { handleInputChange };
