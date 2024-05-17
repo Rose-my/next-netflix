44d992c7cb0fa7acb4a1cc59627404d75dbde049
Rose <120544840+Rose-my@users.noreply.github.com>
2024-05-17 20:26:19 +0900

    fix: commonLayout 제거

--- netflix/src/app/layout.tsx
+++ netflix/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import FooterLine from '@/components/common/FooterLine';
 
 export const metadata: Metadata = {
   title: 'next-netflix',
@@ -14,7 +15,12 @@ export const viewport = 'width=device-width, initial-scale=1.0, maximum-scale=1.
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <section className="children-container">
+          {children}
+          <FooterLine />
+        </section>
+      </body>
     </html>
   );
 }
