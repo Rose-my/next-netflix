a7cca5185079b01475befe55ff5bf00a87b1bf1e
안혜연 <ahy020606@naver.com>
2024-05-17 20:25:17 +0900

    fix: 콘솔 400에러

--- netflix/src/components/search/Movies.tsx
+++ netflix/src/components/search/Movies.tsx
@@ -13,13 +13,16 @@ export default function Movies({ searchedMovies }: MoviesProps) {
         searchedMovies.map((movie: MovieTypes) => (
           <div key={movie.id} className="flex bg-search-gray">
             <article className={'w-[300px] h-[140px] overflow-hidden relative shrink-0 rounded-r-lg'}>
-              <Image
-                alt={movie.title}
-                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                fill
-                className="object-cover"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
+              {movie.poster_path && (
+                <Image
+                  alt={movie.title}
+                  src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                  fill
+                  className="object-cover"
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  priority
+                />
+              )}
             </article>
             <div className="flex justify-between items-center w-full px-4">
               <p className="fonts-searched">{movie.title}</p>
